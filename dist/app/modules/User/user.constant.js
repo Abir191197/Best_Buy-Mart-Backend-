d96eedbbc0ae265c283d3bb1b5df884694c8b9ce
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-12-04 04:07:53 +0600

    Remove  dist from Git

--- dist/app/modules/User/user.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_ROLE = void 0;
-// Define the constant for user roles
-exports.USER_ROLE = {
-    USER: "USER",
-    ADMIN: "ADMIN",
-    VENDOR: "VENDOR",
-};
--- dist/app/utils/sendResponse.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sendResponse = (res, responseData) => {
-    const { statusCode, success, message, data, token } = responseData;
-    res.status(statusCode).json({ success, statusCode, message, data, token });
-};
-exports.default = sendResponse;
