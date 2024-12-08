fa2632405be748f93a1a1a18f9ac566d175d7157
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2024-12-09 02:41:43 +0600

    valid

--- src/app/modules/Payment/payment.service.ts
+++ src/app/modules/Payment/payment.service.ts
@@ -1,24 +1,31 @@
 import { SSLService } from "./payment.utils";
 
 const validatePaymentIntoDB = async (payload: any) => {
+  console.log("Processing IPN Data:", payload);
 
-    console.log("valid payment", payload);
+  //// Check the validation status
+  if (payload?.status !== "VALID") {
+    console.error("Payment validation failed:", );
+    return {
+      message: "Payment Failed!",
+      
+    };
+    }
   
-  // Validate the payment using the SSLService
-  const validationResponse = await SSLService.validatePayment(payload);
+  //  Validate the payment using the SSLCommerz service
+  const validationResponse = await SSLService.validatePayment(payload.val_id);
 
-  // Check the status of the validation response
-//   if (validationResponse?.status  !== "VALID") {
-//     return {
-//       message: "Payment Failed!",
-//     };
-//   }
+  
 
-  // Payment is valid
+  // Payment is valid, proceed to process the order (e.g., update database)
+  console.log("Payment validated successfully:", validationResponse);
+
   return {
     message: "Payment success!",
+    details: validationResponse,
   };
 };
+
 export const PaymentService = {
   validatePaymentIntoDB,
 };
